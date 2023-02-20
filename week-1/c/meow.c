b8aecd920cac5b565eee3739bb93059699d187cf
hoodiemoder <hoodiemoder@pm.me>
2023-02-20 09:58:10 +0100

    Add meow.c

---
+++ week-1/c/meow.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <cs50.h>
+int main(void)
+{
+    //While loop for meowing
+
+    //string meow = ("Meow!");
+    int i = 0;
+    while (i < 3)
+    {
+        printf("Meow\n");
+        i++;
+    }
+
+    //For loop for meowing 3 times
+    for (int j = 0; j < 3; j++)
+    {
+        printf("Meow\n");
+    }
+}
