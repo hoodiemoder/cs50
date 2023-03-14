27fd3bea765c201eeae106a7b15e01e6322d1e21
hoodiemoder <hoodiemoder@pm.me>
2023-03-14 14:04:55 +0100

    Update C folder for week 1 assignment.

--- week-1/c/credit/credit.c
+++ week-1/c/credit/credit.c
@@ -1,58 +1,139 @@
-#include <cs50.h>
-#include <math.h>
 #include <stdio.h>
+#include <math.h>
+#include <cs50.h>
 
-int main(void)
+bool lunhs(int digits, int number_array[], bool valid)
 {
-    long d13 = pow(10, 12);  // 13 digits
-    long d14 = pow(10, 13);  // 14 digits
-    long d15 = pow(10, 14);  // 15 digits
-    long d16 = pow(10, 15);  // 16 digits
-    long d17 = pow(10, 16);  // 17 digits
-
-    long card_number = get_double("Number: ");
-
-    if (card_number >= d13 && card_number < d14)
+    int odd = 0;
+    int even = 0;
+    int even_array[digits];
+    int carry = 0;
+    for (int i = 1; i <= digits; i++)
     {
-        long first_number = card_number / d13;
-        if (first_number == 4)
+        if (i % 2 == 0)
         {
-            printf("Possible Visa\n");
+            even_array[i/2] = number_array[i] *= 2;
         }
         else
         {
-            printf("INVALID\n");
+            odd += number_array[i];
         }
     }
-    else if (card_number >= d15 && card_number < d16)
+    for (int i = 1; i < digits; i++)
     {
-        if (card_number / d14 == 34 || card_number / d14 == 37)
+        if (even_array[i] >= 10)
         {
-            printf("Possible American Express\n");
+            carry += 1;
+            even_array[i] = even_array[i] % 10;
+            even += even_array[i];
         }
         else
         {
-            printf("INVALID\n");
+            even += even_array[i];
         }
     }
-    else if (card_number >= d16 && card_number < d17)
+
+    // Debug: Prints the content of the even_array.
+    //for (int i = 1; i <= digits / 2; i++)
+    //{
+    //    printf("even_array[%i]:%i\n", i, even_array[i]);
+    //}
+
+    int sum = even + odd + carry;
+    printf("sum: %i even: %i odd: %i carry: %i\n", sum, even, odd, carry);
+
+    if (sum % 10 == 0)
     {
-        if (card_number / d16 == 4)
-        {
-            printf("Possible Visa.");
-        }
-        else if (card_number / d15 == (> 51 && < 56))
-        {
-            printf("Possible Mastercard.");
-        }
-        else
-        {
-            printf("INVALID\n");
-        }
+        return true;
     }
     else
     {
-        // If the number doesn't match the number of digits, it is invalid.
-        printf("INVALID\n");
+        return false;
     }
 }
+
+int main(void)
+{
+    string brand;
+    bool valid = false;
+    // Gets user's credit card number.
+    long card_number = get_long("Card number: ");
+
+    // Create an array for storing credit card number.
+    int digits = floor(log10(card_number) + 1);
+    int number_array[digits];
+
+    for (int i = 1; i <= digits; i++)
+    {
+        number_array[i] = card_number % 10;
+        card_number /= 10;
+    }
+
+    // Debug: Prints the content of the array.
+    //for (int i = 1; i <= digits; i++)
+    //{
+    //    printf("number_array[%i]:%i\n", i, number_array[i]);
+    //}
+
+    // Switch case for different card lenght.
+    switch (digits)
+    {
+        case 13:
+            if (number_array[13] == 4)
+            {
+                brand = "VISA";
+                valid = lunhs(digits, number_array, valid);
+                if (valid == true)
+                {
+                    printf("%s\n", brand);
+                    break;
+                }
+            }
+        case 15:
+                if (number_array[15] == 3)
+                {
+                    if (number_array[14] == 4 || number_array[14] ==  7)
+                    {
+                        brand = "AMEX";
+                        valid = lunhs(digits, number_array, valid);
+                        if (valid == true)
+                        {
+                            printf("%s\n", brand);
+                            break;
+                        }
+                    }
+                }
+        case 16:
+            if (number_array[16] == 4)
+            {
+                brand = "VISA";
+                valid = lunhs(digits, number_array, valid);
+
+                if (valid == true)
+                {
+                    printf("%s\n", brand);
+                    break;
+                }
+            }
+            else if (number_array[16] == 5)
+            {
+                if (number_array[15] >= 1 && number_array[15] <= 5)
+                {
+                    brand = "MASTERCARD";
+                    valid = lunhs(digits, number_array, valid);
+
+                    if (valid == true)
+                    {
+                        printf("%s\n", brand);
+                        break;
+                    }
+                }
+            }
+        default:
+            if (valid == false)
+            {
+            printf("INVALID\n");
+            }
+    }
+}
+
